83b13bc06a1d509bafa8d2252119c74c3be4c95c
Eleonora412 <releonora58@gmail.com>
2020-07-20 10:45:45 +0200

    primo commit

---
+++ src/it/hello/maven/HelloMaven.java
@@ -0,0 +1,26 @@
+package it.hello.maven;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hellomaven")
+public class HelloMaven extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public HelloMaven() {
+      
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>Hello World!</h1><body><html>");
+	}
+
+}
